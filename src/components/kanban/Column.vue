b96352dd3381112a6c538382eb5017891b977f36
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-11-11 20:23:34 +0300

    Markup and styling kanban components: board, column and card.

---
+++ src/components/kanban/Column.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="column">
+
+    <div class="column-header">
+      <p>IN PROGRESS</p>
+    </div>
+
+    <div class="column-content">
+      <card></card>
+      <card></card>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Card from '@/components/kanban/Card'
+export default {
+  name: 'Column',
+  components: {
+    Card
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.column {
+  display: inline-block;
+  margin-left: 15px;
+  margin-right: 50px;
+  margin-bottom: 15px;
+  width: 300px;
+}
+
+.column:last-child {
+  margin-right: 15px;
+}
+
+.column-header {
+  color: white;
+  text-shadow: 0 0 4px #4b5263;
+  text-transform: uppercase;
+  padding: 10px 10px;
+  background-color: #2a92bf;
+}
+
+.column-header p {
+  margin: 0;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+}
+
+.column-content {
+  background-color: #2f2f36;
+  padding: 15px;
+}
+</style>
